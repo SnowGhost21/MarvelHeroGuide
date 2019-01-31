fcbe78ff35017c019637ed03dd9a2669926983c7
Diego <domdiegobc@gmail.com>
2019-01-31 19:51:13 -0200

    [DEV]Created Splash Screen

--- app/src/main/java/com/diegocunha/marvelheroguide/view/MainActivity.kt
+++ app/src/main/java/com/diegocunha/marvelheroguide/view/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.AppTheme)
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setSupportActionBar(binding.toolbar)
     }
---
+++ app/src/main/java/com/diegocunha/marvelheroguide/view/start/SplashFragment.kt
@@ -0,0 +1,22 @@
+package com.diegocunha.marvelheroguide.view.start
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.diegocunha.marvelheroguide.R
+
+class SplashFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+    }
+}
